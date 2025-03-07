aeebe2d471fb62f351612754130fe572d725755d
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-03-08 04:34:00 +0530

    Handling Loaded Video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,6 +10,9 @@ const Hero = () => {
   const totalVideos = 4
   const nextVideoRef = useRef(null)
 
+  const handleVideoLoaded  =() => {
+    setLoadedVideos((prev) => prev + 1)
+  }
 
   const handleMiniVdClick = () => {
     setHasClicked(true)
@@ -24,7 +27,7 @@ const Hero = () => {
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div onClick={handleMiniVdClick} className="origin-center">
+            <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
               <video
                 ref={nextVideoRef}
                 src={getVideoSource(currentIndex + 1)}
@@ -32,6 +35,7 @@ const Hero = () => {
                 muted
                 id="current-video"
                 className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoaded}
               />
             </div>
           </div>
