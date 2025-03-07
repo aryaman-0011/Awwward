83cc189a7a46c50604ecbfa61c53bd53a3639371
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-03-08 04:57:15 +0530

    Added video in background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   const totalVideos = 3
   const nextVideoRef = useRef(null)
 
-  const handleVideoLoaded = () => {
+  const handleVideoLoad = () => {
     setLoadedVideos((prev) => prev + 1)
   }
 
@@ -37,15 +37,34 @@ const Hero = () => {
             <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
               <video
                 ref={nextVideoRef}
-                src={getVideoSource(currentIndex + 1)}
+                src={getVideoSource(upcomingVideoIndex)}
                 loop
                 muted
                 id="current-video"
                 className="size-64 origin-center scale-150 object-cover object-center"
-                onLoadedData={handleVideoLoaded}
+                onLoadedData={handleVideoLoad}
               />
             </div>
           </div>
+
+          <video
+            ref={nextVideoRef}
+            src={getVideoSource(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+
+          <video
+          src={getVideoSource(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+          autoPlay
+          loop
+          muted
+          className="absolute left-0 top-0 size-full object-cover object-center"
+          onLoadedData={handleVideoLoad}
+          />
         </div>
       </div>
     </div>
