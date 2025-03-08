bd9cc173703da6e237ab3fdcfe7311b551840163
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-03-09 01:04:46 +0530

    Added Logo and Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useRef } from 'react'
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    const navContainerRef = useRef(null)
+
+    return (
+        <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
+            <header className='absolute top-1/2 w-full -translate-y-1/2'>
+                <nav className='flex size-full items-center justify-between p-4'>
+                    <div className='flex items-center gap-7'>
+                        <img
+                            src="/img/logo.png"
+                            alt="logo"
+                            className='w-10'
+                        />
+                    </div>
+                </nav>
+            </header>
+
+        </div>
+    )
 }
 
 export default NavBar
