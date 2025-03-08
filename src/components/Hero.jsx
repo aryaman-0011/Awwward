8440ad7b5db332f809c3ca60311859c46815b450
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-03-08 06:41:11 +0530

    Animating using gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
-import { useRef } from "react"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import Button from "./Button"
 import { TiLocationArrow } from "react-icons/ti"
+import { useGSAP } from "@gsap/react"
+import gsap from 'gsap'
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1)
@@ -28,6 +29,30 @@ const Hero = () => {
 
     setCurrentIndex(upcomingVideoIndex)
   }
+
+  useGSAP(() => {
+    if (hasClicked) {
+      gsap.set('#next-video', { visibility: 'visible' })
+
+      gsap.to('#next-video', {
+        transformOrigin: 'center center',
+        scale: 1,
+        width: '100%',
+        height: '100%',
+        duration: 1,
+        ease: 'power1.inOut',
+        onStart: () => nextVideoRef.current.play(),
+      })
+
+      gsap.from('#current-video', {
+        transformOrigin: 'center center',
+        scale: 0,
+        duration: 1.5,
+        ease: 'power1.inOut',
+
+      })
+    }
+  }, { dependencies: [currentIndex], revertOnUpdate: true })
 
   const getVideoSource = (index) => `videos/hero-${index}.mp4`
 
@@ -61,7 +86,7 @@ const Hero = () => {
 
           <video
             src={getVideoSource(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-            // autoPlay
+            autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
