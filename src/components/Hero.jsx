0d2983d51027122d9154895047850518e4a8fc0d
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-03-08 14:36:00 +0530

    Animating scroll

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react"
 import Button from "./Button"
 import { TiLocationArrow } from "react-icons/ti"
 import { useGSAP } from "@gsap/react"
-import gsap from 'gsap'
+import gsap from "gsap"
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1)
@@ -53,6 +53,26 @@ const Hero = () => {
       })
     }
   }, { dependencies: [currentIndex], revertOnUpdate: true })
+
+  useGSAP(() => {
+    gsap.set('#video-frame', {
+      clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+      borderRadius: '0 0 40% 10%',
+    })
+
+    gsap.from('#video-frame', {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius: '0 0 0 0',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: '#video-frame',
+        start: 'center center',
+        end: 'bottom center',
+        scrub: true
+      }
+
+    })
+  })
 
   const getVideoSource = (index) => `videos/hero-${index}.mp4`
 
