1c12444b910ad1ad3311ff886bd08a917ac2de21
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-03-16 02:18:49 +0530

    Added text

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Features = () => {
-  return (
-    <div>Features</div>
-  )
+    return (
+        <section className='bg-black pb-52'>
+            <div className='container mx-auto px-3 md:px-10'>
+                <div className='px-5 py-32 '>
+                    <p className='font-circular-web text-lg text-blue-50'>Into the Metagame layer</p>
+                </div>
+                <p className='max-w-md font-circular-web text-lg text-blue-50 opacity-50'>
+                    Immerse yourself in a rich and ever-expanding universe where a vibrant array of products converge into an interconnected overlay experience on your world.
+                </p>
+            </div>
+            
+        </section>
+    )
 }
 
 export default Features
