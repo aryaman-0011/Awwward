b1ed5d407a550beb02317c8ca08f23e5e0ebbe03
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-03-17 23:27:14 +0530

    Replacing div with Bentotilt

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,6 +1,6 @@
 import { TiLocationArrow } from "react-icons/ti"
 
-const BentoTilt = ({children, className=''}) => {
+const BentoTilt = ({ children, className = '' }) => {
     return (
         <div>
             {children}
@@ -40,35 +40,35 @@ const Features = () => {
                         Immerse yourself in a rich and ever-expanding universe where a vibrant array of products converge into an interconnected overlay experience on your world.
                     </p>
                 </div>
-                <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
+                <BentoTilt className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
                     <BentoCard
                         src="videos/feature-1.mp4"
                         title={<>radi<b>n</b>t</>}
                         description="A cross-platform metagame app, turning your activities across web2 and web3 games into a rewarding adventure."
                     />
-                </div>
+                </BentoTilt>
                 <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
-                    <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+                    <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
                         <BentoCard
                             src="videos/feature-2.mp4"
                             title={<>zig<b>m</b>a</>}
                             description="An anime and gaming inspired NFT collection - the IP primed for expansion."
                         />
-                    </div>
-                    <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+                    </BentoTilt>
+                    <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
                         <BentoCard
                             src="videos/feature-3.mp4"
                             title={<>n<b>e</b>xus</>}
                             description="A gamified social hub, adding a new dimension of play to social interaction for web3 communities."
                         />
-                    </div>
-                    <div className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+                    </BentoTilt>
+                    <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
                         <BentoCard
                             src="videos/feature-4.mp4"
                             title={<>az<b>u</b>l</>}
                             description="A cross-world AI Agent - elevating your gameplay to be more fun and productive"
                         />
-                    </div>
+                    </BentoTilt>
                     <div className="bento_tilt_2">
                         <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
                             <h1 className="bento-title special-font max-w-64 text-black">
@@ -77,15 +77,15 @@ const Features = () => {
                             <TiLocationArrow className="m-5 scale-[5] self-end" />
                         </div>
                     </div>
-                    <div className="bento-tilt_2">
+                    <BentoTilt className="bento-tilt_2">
                         <video
-                        src="/videos/feature-5.mp4"
-                        loop
-                        muted
-                        autoPlay
-                        className="size-full object-cover object-center"
+                            src="/videos/feature-5.mp4"
+                            loop
+                            muted
+                            autoPlay
+                            className="size-full object-cover object-center"
                         />
-                    </div>
+                    </BentoTilt>
                 </div>
             </div>
         </section>
