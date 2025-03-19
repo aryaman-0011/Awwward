3c3bf192f38316c22265132750260ba20e34c1fa
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-03-19 16:31:49 +0530

    Added Animation to image

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import AnimatedTitle from './AnimatedTitle'
+import gsap from 'gsap'
 
 const Story = () => {
+  const frameRef = useRef('null')
+
+  const handleMouseLeave = () => {
+    const element = frameRef.current
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0, rotateY: 0,
+      ease: 'power1.inOut'
+    })
+  }
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e
+    const element = frameRef.current
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect()
+    const x = clientX - rect.left
+    const y = clientY - rect.top
+
+    const centerX = rect.width / 2
+    const centerY = rect.height / 2
+
+    const rotateX = ((y - centerY) / centerY) * -10
+    const rotateY = ((x - centerX) / centerX) * 10
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX, rotateY,
+      transformPerspective: 500,
+      ease: 'power1.inOut'
+    })
+
+  }
+
   return (
     <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
       <div className='flex size-full flex-col items-center py-10 pb-24'>
@@ -19,6 +56,11 @@ const Story = () => {
             <div className='story-img-mask'>
               <div className='story-img-content'>
                 <img
+                  ref={frameRef}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
                   src="/img/entrance.webp"
                   alt='entrance'
                   className='object-contain'
