0fe532059265ff304609c4954befe2a1d5784461
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-03-22 23:46:45 +0530

    Added images

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ImageClipBox = (src, clipClass) => (
+const ImageClipBox = ({ src, clipClass }) => (
   <div className={clipClass}>
     <img src={src} />
   </div>
-) 
+)
 
 const Contact = () => {
   return (
@@ -15,6 +15,25 @@ const Contact = () => {
             clipClass="contact-clip-path-1"
             src="img/contact-1.webp"
           />
+          <ImageClipBox
+            clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
+            src="img/contact-2.webp"
+          />
+        </div>
+
+        <div className='absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80'>
+          <ImageClipBox
+            src="img/swordman-partial.webp"
+            clipClass="absolute md:scale-125"
+          />
+          <ImageClipBox
+            src="img/swordman.webp"
+            clipClass="sword-man-clip-path md:scale-125"
+          />
+        </div>
+
+        <div>
+          
         </div>
       </div>
     </div>
